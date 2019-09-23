db11894295e690cff61ef52b15e6bc79e4c2e45e
Manik <mggmanik@gmail.com>
2019-09-23 23:39:17 +0530

    added modal for bookings

--- src/app/bookings/bookings.module.ts
+++ src/app/bookings/bookings.module.ts
@@ -1,26 +1,27 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {Routes, RouterModule} from '@angular/router';
 
-import { IonicModule } from '@ionic/angular';
+import {IonicModule} from '@ionic/angular';
 
-import { BookingsPage } from './bookings.page';
+import {BookingsPage} from './bookings.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: BookingsPage
-  }
+    {
+        path: '',
+        component: BookingsPage
+    }
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  declarations: [BookingsPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        RouterModule.forChild(routes)
+    ],
+    declarations: [BookingsPage]
 })
-export class BookingsPageModule {}
+export class BookingsPageModule {
+}
